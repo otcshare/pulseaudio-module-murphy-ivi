dcbeb5994710d104cf0fe0cbf156381b29b264f0
Janos Kovacs <jankovac503@gmail.com>
2012-12-10 03:17:48 +0200

    configuration: add application classes and volume control to basic script

--- murphy/murphy-ivi.lua
+++ murphy/murphy-ivi.lua
@@ -20,3 +20,74 @@ routing_group {
     compare = builtin.method.compare_phone
 }
 
+application_class {
+    node_type = node.phone,
+    priority = 4,
+    route = {
+        input = routing_group.phone_input,
+        output = routing_group.phone_output
+    }
+}
+
+application_class {
+    node_type = node.radio,
+    priority = 1,
+    route = {
+        output = routing_group.default_output
+    }
+}
+
+application_class {
+    node_type = node.player,
+    priority = 1,
+    route = {
+        output = routing_group.default_output
+    }
+}
+
+application_class {
+    node_type = node.navigator,
+    priority = 2,
+    route = {
+        output = routing_group.default_output
+    }
+}
+
+application_class {
+    node_type = node.game,
+    priority = 3,
+    route = {
+        output = routing_group.default_output
+    }
+}
+
+application_class {
+    node_type = node.browser,
+    priority = 1,
+    route = {
+        output = routing_group.default_output
+    }
+}
+
+application_class {
+    node_type = node.event,
+    priority = 5,
+    route = {
+        output = routing_group.default_output
+    }
+}
+
+volume_limit {
+    name = "speed_adjust",
+    type = volume_limit.generic,
+    limit = -10;
+    calculate = builtin.method.volume_correct
+}
+
+volume_limit {
+    name = "suppress",
+    type = volume_limit.class,
+    limit = -20;
+    node_type = { node.phone, node.navigator },
+    calculate = builtin.method.volume_supress
+}
