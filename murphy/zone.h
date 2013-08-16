e1b20dad61f4755e8ad0c026984088b1ea44b190
Janos Kovacs <jankovac503@gmail.com>
2013-08-16 19:38:38 +0300

    zone: getting zones both by name and index

--- murphy/zone.h
+++ murphy/zone.h
@@ -33,7 +33,8 @@ pa_zoneset *pa_zoneset_init(struct userdata *);
 void pa_zoneset_done(struct userdata *);
 
 int pa_zoneset_add_zone(struct userdata *, const char *, uint32_t);
-mir_zone *pa_zoneset_get_zone(struct userdata *, const char *);
+mir_zone *pa_zoneset_get_zone_by_name(struct userdata *, const char *);
+mir_zone *pa_zoneset_get_zone_by_index(struct userdata *, uint32_t);
 
 void pa_zoneset_update_module_property(struct userdata *);
 
