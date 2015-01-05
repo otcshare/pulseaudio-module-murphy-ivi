f0b66984855483b801be0646a745131b9734fbc6
Jaska Uimonen <jaska.uimonen@helsinki.fi>
2015-01-05 11:41:39 +0200

    add appid property back to resource attributes
    Change-Id: Ia0cd03137f41df2cff249343f0073daec8a132a3

--- murphy/murphy-ivi.lua
+++ murphy/murphy-ivi.lua
@@ -135,7 +135,8 @@ audio_resource {
     attributes = {
        role = {"media.role", mdb.string, "music"},
        pid  = {"application.process.id", mdb.string, "<unknown>"},
-       name = {"resource.set.name", mdb.string, "<unknown>"}
+       name = {"resource.set.name", mdb.string, "<unknown>"},
+       appid = {"resource.set.appid", mdb.string, "<unknown>"}
     }
 }
 
